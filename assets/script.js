ef8679a3d058c521674b2431bff4322c95a2a8e6
DanielWestiner <daniel.westiner@gmail.com>
2021-12-20 21:34:54 -0500

    updated js with function to get coordinates, working on function to get current weather and save into local storage

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,71 @@
 // Setting up the One Call Open Weather API
 
-let weatherAPI = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2824096d7fe149eff6336b2232af2c6c";
+let weatherKey =
+  "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2824096d7fe149eff6336b2232af2c6c";
+
+// Set up OpenWeather Direct Geocoding API, uses this API call:
+// http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid=2824096d7fe149eff6336b2232af2c6c
+let cityName
 
 
+document.getElementById("searchBtn").addEventListener("click", searchCity);
+function searchCity(event) {
+  event.preventDefault ();
+
+  let cityName = citySearch.value
+
+  if (cityName === undefined) {
+    alert("Please enter a valid city")
+  }
+
+}
+
+
+
+let geoCoord = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=2824096d7fe149eff6336b2232af2c6c"
+
+// Get the coordinates of the city from Openweather Geocoding API
+
+function coordinates () {
+  return fetch(geoCoord)
+    .then (function (response) {
+      if (response.ok) {
+        return response.json()
+      } else {
+        console.error("could not get coordinates!");
+      }
+    })
+    .then (function (data) {
+      longitude  = data.coord.lon;
+      latitude = data.coord.lat
+      if (data.ok) {
+        return {
+          longitude: data.coord.lon,
+          latitude: data.coord.lat
+        }
+      }
+    })
+  }
+
+
+
+
+
+
+
+let weatherForecast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude 
+  + "&lon=" + longitude + 
+  "&exclude=minutely,hourly&units=imperial&appid=2824096d7fe149eff6336b2232af2c6c";
+
+// Get the weather data
+
+function weather (input) {
+  return fetch (weatherForecast)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json()
+      }
+    }
+    )
+}
+    
